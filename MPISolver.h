97873c4abe68f2b10f259cd390a794cfa258dd28
Enes Kalinsazlioglu <ekalinsaz@gmail.com>
2024-10-28 19:03:57 -0400

    working MPI stich

--- MPISolver.h
+++ MPISolver.h
@@ -3,15 +3,73 @@
 using namespace std;
 
 // MPI implementation
-void TSP::MPI_solver(Algorithm algorithm, int world_rank, int world_size)
+void TSP::MPI_solver(Algorithm algorithm, int my_world_rank, int world_size)
 {
+    ///// MPI SETUP /////
+    int ndims = 2;
+    int dims[ndims], my_coord[ndims];
+    int wrap_around[ndims], reorder;
+    int my_cart_rank;
+    int nrows, ncols;
+    int local_val, row_sum = 0;
+    int col_val, col_sum = 0;
+
+    MPI_Comm comm2D;     // Cartesian communicator
+    MPI_Comm comm1D_row; // Communicator for row split
+    MPI_Comm comm1D_col; // Communicator for col split
+
+    // For split
+    int my_row_rank;
+    int my_col_rank;
+    int free_coords[ndims];
+    int my_row_coord[ndims];
+    int my_col_coord[ndims];
+
+    // Create a Cartesian communicator
+    nrows = ncols = sqrt(world_size);
+    dims[0] = nrows;                     // number of rows
+    dims[1] = ncols;                     // number of columns
+    wrap_around[0] = wrap_around[1] = 1; // periodic shift is .true.
+    reorder = 1;                         // reorder is .true.
+    MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, wrap_around, reorder, &comm2D);
+
+    /* find my coordinates in the cartesian communicator group */
+    MPI_Cart_coords(comm2D, my_world_rank, ndims, my_coord);
+
+    /* use my cartesian coordinates to find my rank in cartesian group*/
+    MPI_Cart_rank(comm2D, my_coord, &my_cart_rank);
+
+    // Split comm2D into a 1D row-based communicator: comm1D_row
+    free_coords[0] = 0; /* rows */
+    free_coords[1] = 1; /* cols */
+    MPI_Cart_sub(comm2D, free_coords, &comm1D_row);
+
+    /* get my_row_rank in my comm1D_row group */
+    MPI_Comm_rank(comm1D_row, &my_row_rank);
+
+    /* use my_row_rank to find my coordinates in my comm1D_row group */
+    MPI_Cart_coords(comm1D_row, my_row_rank, 1, my_row_coord);
+
+    // Split comm2D into a 1D col-based communicator: comm1D_col
+    free_coords[0] = 1; /* rows */
+    free_coords[1] = 0; /* cols */
+    MPI_Cart_sub(comm2D, free_coords, &comm1D_col);
+
+    /* get my_col_rank in my comm1D_col group */
+    MPI_Comm_rank(comm1D_col, &my_col_rank);
+
+    /* use my_col_rank to find my coordinates in my comm1D_col group */
+    MPI_Cart_coords(comm1D_col, my_col_rank, 1, my_col_coord);
+    ///// MPI SETUP /////
+
+    //~~~~~~~~ Parallel Work (begin) ~~~~~~~~//
 
     // Regular array to store the results
-    City *path;
+    City *full_raw_path;
     int total_path_length;
 
     // Create a new array to store the path
-    path = new City[world_size * cities_per_block];
+    full_raw_path = new City[world_size * cities_per_block];
 
     // Create a new vector of cities for the thread
     vector<City> cities;
@@ -19,10 +77,10 @@ void TSP::MPI_solver(Algorithm algorithm, int world_rank, int world_size)
     int **emst_distances = new int *[cities_per_block];
     std::vector<std::vector<double>> dp_distances = std::vector<std::vector<double>>(cities_per_block, std::vector<double>(cities_per_block));
 
-    thread_populate_block(world_rank, cities);
+    thread_populate_block(my_world_rank, cities);
 
     if (DEBUG)
-        print_thread_cities(world_rank, cities);
+        print_thread_cities(my_world_rank, cities);
 
     if (algorithm == TSP::Algorithm::DP)
     {
@@ -36,7 +94,7 @@ void TSP::MPI_solver(Algorithm algorithm, int world_rank, int world_size)
         }
 
         if (DEBUG)
-            print_thread_distances_DP(world_rank, dp_distances);
+            print_thread_distances_DP(my_world_rank, dp_distances);
 
         // Find the path and length for the thread
         std::list<int> tour;
@@ -64,7 +122,7 @@ void TSP::MPI_solver(Algorithm algorithm, int world_rank, int world_size)
         }
 
         if (DEBUG)
-            print_thread_distances_EMST(world_rank, emst_distances);
+            print_thread_distances_EMST(my_world_rank, emst_distances);
 
         // Find the path and length for the thread
         std::vector<int> tour;
@@ -80,23 +138,84 @@ void TSP::MPI_solver(Algorithm algorithm, int world_rank, int world_size)
     }
 
     // Convert the tour to a simple array of city ids
-    City *tour_simple = new City[cities_per_block];
+    City *block_path = new City[cities_per_block];
     for (int i = 0; i < cities_per_block; i++)
     {
-        tour_simple[i] = tour_with_coordinates[i];
+        block_path[i] = tour_with_coordinates[i];
     }
 
-    // Sync all the threads
-    // MPI_Barrier(MPI_COMM_WORLD);
+    // MOCK STICHING (linear)
+    local_val = my_world_rank;
+    int recv_val;
+
+    // Stich together block TSP solutions row-wise, each row separately in parallel
+    for (int cur_sticher = 1; cur_sticher < ncols; cur_sticher++)
+    {
+        // SEND
+        if (my_row_rank == cur_sticher - 1) // if I am the sender (one before the processor that stiches)
+        {
+            int destination = my_row_rank + 1;
+            if (my_row_rank == 0)
+                MPI_Send(&local_val, 1, MPI_INT, destination, 0, comm1D_row);
+            else
+                MPI_Send(&row_sum, 1, MPI_INT, destination, 0, comm1D_row);
+        }
+        // RECEIVE
+        if (my_row_rank == cur_sticher) // if I am the sticher
+        {
+            int source = my_row_rank - 1;
+            MPI_Recv(&recv_val, 1, MPI_INT, source, 0, comm1D_row, MPI_STATUS_IGNORE);
+            // STICH
+            row_sum = local_val + recv_val;
+        }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // sum col values across cols one by one
+    if (my_row_rank == ncols - 1) // only the last processors in each row will stich
+    {
+        for (int cur_sticher = 1; cur_sticher < nrows; cur_sticher++)
+        {
+            // SEND
+            if (my_col_rank == cur_sticher - 1) // if I am the sender (one before the processor that stiches)
+            {
+                int destination = my_col_rank + 1;
+                if (my_col_rank == 0)
+                    MPI_Send(&row_sum, 1, MPI_INT, destination, 0, comm1D_col);
+                else
+                    MPI_Send(&col_sum, 1, MPI_INT, destination, 0, comm1D_col);
+            }
+            // RECEIVE
+            if (my_col_rank == cur_sticher) // if I am the sticher
+            {
+                int source = my_col_rank - 1;
+                MPI_Recv(&recv_val, 1, MPI_INT, source, 0, comm1D_col, MPI_STATUS_IGNORE);
+                // STICH
+                col_sum = row_sum + recv_val;
+            }
+        }
+    }
+
+    if (my_row_rank == ncols - 1)
+        printf("P%d: RowSum: %d\n", my_world_rank, row_sum);
+
+    if (my_world_rank == world_size - 1)
+        printf("P%d: ColSum: %d\n", my_world_rank, col_sum);
+
+    printf("WRank: %d, RowRank: %d, ColRank:%d, Coord: (%d, %d), RowSum: %d\n",
+           my_world_rank, my_row_rank, my_col_rank, my_coord[0], my_coord[1], row_sum);
+
+    //~~~~~~~~ Parallel Work (end) ~~~~~~~~//
 
     // Gather all the results
-    MPI_Gather(tour_simple, cities_per_block * sizeof(City), MPI_BYTE, path, cities_per_block * sizeof(City), MPI_BYTE, 0, MPI_COMM_WORLD);
+    MPI_Gather(block_path, cities_per_block * sizeof(City), MPI_BYTE, full_raw_path, cities_per_block * sizeof(City), MPI_BYTE, 0, MPI_COMM_WORLD);
 
     // Gather all the path lengths
     MPI_Reduce(&pathLength, &total_path_length, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
     // Stich together the paths from each thread
-    if (world_rank == 0)
+    if (my_world_rank == 0)
     {
         final_tour.clear();
         // At the end of a row, continue adding the next row in the reverse order
@@ -112,18 +231,18 @@ void TSP::MPI_solver(Algorithm algorithm, int world_rank, int world_size)
                     // Insert the block's path into the total path
                     for (int i = 0; i < cities_per_block; i++)
                     {
-                        final_tour.push_back(path[process_id * cities_per_block + i]);
+                        final_tour.push_back(full_raw_path[process_id * cities_per_block + i]);
                     }
 
                     // Add the distance between the last city of the previous block and the first city of the current block
                     if (column != grid_side - 1) // unless last column
                     {
                         // Add the distance between next block's last city and current block's first city
-                        total_path_length += calculate_distance(path[((process_id + 1) * cities_per_block) + (cities_per_block - 1)], path[process_id * cities_per_block]);
+                        total_path_length += calculate_distance(full_raw_path[((process_id + 1) * cities_per_block) + (cities_per_block - 1)], full_raw_path[process_id * cities_per_block]);
                     }
                     else // if last columns
                     {
-                        total_path_length += calculate_distance(path[((process_id - grid_side) * cities_per_block) + (cities_per_block - 1)], path[process_id * cities_per_block]);
+                        total_path_length += calculate_distance(full_raw_path[((process_id - grid_side) * cities_per_block) + (cities_per_block - 1)], full_raw_path[process_id * cities_per_block]);
                     }
                 }
             }
@@ -135,16 +254,16 @@ void TSP::MPI_solver(Algorithm algorithm, int world_rank, int world_size)
                     // Insert the block's path into the total path
                     for (int i = 0; i < cities_per_block; i++)
                     {
-                        final_tour.push_back(path[process_id * cities_per_block + i]);
+                        final_tour.push_back(full_raw_path[process_id * cities_per_block + i]);
                     }
                     // Add the distance between the last city of the previous block and the first city of the current block
                     if (column != 0) // unless first column
                     {
-                        total_path_length += calculate_distance(path[((process_id - 1) * cities_per_block) + (cities_per_block - 1)], path[process_id * cities_per_block]);
+                        total_path_length += calculate_distance(full_raw_path[((process_id - 1) * cities_per_block) + (cities_per_block - 1)], full_raw_path[process_id * cities_per_block]);
                     }
                     else if (row != 0) // unless first row but first column
                     {
-                        total_path_length += calculate_distance(path[((process_id - grid_side) * cities_per_block) + (cities_per_block - 1)], path[process_id * cities_per_block]);
+                        total_path_length += calculate_distance(full_raw_path[((process_id - grid_side) * cities_per_block) + (cities_per_block - 1)], full_raw_path[process_id * cities_per_block]);
                     }
                 }
             }
@@ -152,14 +271,20 @@ void TSP::MPI_solver(Algorithm algorithm, int world_rank, int world_size)
 
         // Add the distance between the last city and the first city
         City last_city = final_tour.back();
-        total_path_length += calculate_distance(last_city, path[0]);
+        total_path_length += calculate_distance(last_city, full_raw_path[0]);
 
         // Add first city to end of tour
-        final_tour.push_back(path[0]);
+        final_tour.push_back(full_raw_path[0]);
 
         // Store the final tour
         pathLength = total_path_length;
     }
+
+    // Finalize the MPI environment
+    MPI_Comm_free(&comm2D);
+    MPI_Comm_free(&comm1D_row);
+    MPI_Comm_free(&comm1D_col);
+    MPI_Finalize();
 
     // // Free memory
     // if (Algorithm::DP == algorithm)
--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@
 #include "MyThread.h"
 #define CPMS CLOCKS_PER_SEC * 1000
 
-#define NUM_THREADS 4
+#define NUM_THREADS 16
 
-#define TOTAL_CITY_COUNT (NUM_THREADS * 4096)
+#define TOTAL_CITY_COUNT (NUM_THREADS * 256)
 
 #define BLOCK_SIDE_LENGTH 100
 
@@ -21,29 +21,6 @@ int main(int argc, char **argv)
 	int mode_flag = 0;
 	int algo_flag = 0;
 
-	///// MPI SETUP /////
-	int ndims = 2;
-	int dims[ndims], my_coord[ndims];
-	int wrap_around[ndims], reorder;
-	int my_cart_rank;
-	int nrows, ncols;
-	int cnt, rlen;
-	int row_val, row_sum = 0, row_root = -999;
-	int col_val, col_sum = 0, col_root = -999;
-
-	char name[200], nameout[200], rname[100];
-
-	MPI_Comm comm2D;	 // Cartesian communicator
-	MPI_Comm comm1D_row; // Communicator for row split
-	MPI_Comm comm1D_col; // Communicator for col split
-
-	// For split
-	int my_row_rank;
-	int my_col_rank;
-	int free_coords[ndims];
-	int my_row_coord[ndims];
-	int my_col_coord[ndims];
-
 	// Initialize MPI
 	MPI_Init(&argc, &argv);
 
@@ -54,91 +31,6 @@ int main(int argc, char **argv)
 	// Get the rank of the process
 	int my_world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_world_rank);
-
-	// /* get the name of communicator */
-	// nameout[0] = 0;
-	// MPI_Comm_get_name(MPI_COMM_WORLD, nameout, &rlen);
-	// if (my_world_rank == 0)
-	// 	printf("Name of comm world is: %s\n", nameout);
-	// fflush(stdout);
-
-	// Create a Cartesian communicator
-	nrows = ncols = sqrt(world_size);
-	dims[0] = nrows;					 // number of rows
-	dims[1] = ncols;					 // number of columns
-	wrap_around[0] = wrap_around[1] = 1; // periodic shift is .true.
-	reorder = 1;						 // reorder is .true.
-	MPI_Cart_create(MPI_COMM_WORLD, ndims, dims, wrap_around, reorder, &comm2D);
-
-	/* set the name of cartesian communicator */
-	strcpy(name, "comm2D");
-	MPI_Comm_set_name(comm2D, name);
-	nameout[0] = 0;
-
-	// /* get the name of communicator */
-	// MPI_Comm_get_name(comm2D, nameout, &rlen);
-	// if (my_world_rank == 0)
-	// 	printf("Name of comm world is: %s\n", nameout);
-	// fflush(stdout);
-
-	/* find my coordinates in the cartesian communicator group */
-	MPI_Cart_coords(comm2D, my_world_rank, ndims, my_coord);
-
-	/* use my cartesian coordinates to find my rank in cartesian group*/
-	MPI_Cart_rank(comm2D, my_coord, &my_cart_rank);
-
-	// Split comm2D into a 1D row-based communicator: comm1D_row
-	free_coords[0] = 0; /* rows */
-	free_coords[1] = 1; /* cols */
-	MPI_Cart_sub(comm2D, free_coords, &comm1D_row);
-
-	/* get my_row_rank in my comm1D_row group */
-	MPI_Comm_rank(comm1D_row, &my_row_rank);
-
-	/* use my_row_rank to find my coordinates in my comm1D_row group */
-	MPI_Cart_coords(comm1D_row, my_row_rank, 1, my_row_coord);
-
-	/* set the name of cartesian communicator */
-	if (my_row_rank == 0)
-	{
-		row_root = my_world_rank;
-	}
-
-	MPI_Bcast(&row_root, 1, MPI_INT, 0, comm1D_row);
-
-	// Split comm2D into a 1D col-based communicator: comm1D_col
-	free_coords[0] = 1; /* rows */
-	free_coords[1] = 0; /* cols */
-	MPI_Cart_sub(comm2D, free_coords, &comm1D_col);
-
-	/* get my_col_rank in my comm1D_col group */
-	MPI_Comm_rank(comm1D_col, &my_col_rank);
-
-	/* use my_col_rank to find my coordinates in my comm1D_col group */
-	MPI_Cart_coords(comm1D_col, my_col_rank, 1, my_col_coord);
-
-	/* set the name of cartesian communicator */
-	if (my_col_rank == 0)
-	{
-		col_root = my_world_rank;
-	}
-	MPI_Bcast(&col_root, 1, MPI_INT, 0, comm1D_col);
-
-	/* test row com: row_sum processor ranks across rows */
-	if (my_coord[1] == 0)
-		row_val = my_coord[0];
-	else
-		row_val = -1;
-	MPI_Bcast(&row_val, 1, MPI_INT, 0, comm1D_row);
-	MPI_Reduce(&my_world_rank, &row_sum, 1, MPI_INT, MPI_SUM, 0, comm1D_row);
-	MPI_Bcast(&row_sum, 1, MPI_INT, 0, comm1D_row);
-
-	printf("WRank: %d, RowRank: %d, ColRank:%d, Coord: (%d, %d), RowRoot: %d, ColRoot: %d, RowSum: %d\n",
-		   my_world_rank, my_row_rank, my_col_rank, my_coord[0], my_coord[1], row_root, col_root, row_sum);
-
-	MPI_Barrier(MPI_COMM_WORLD);
-
-	///// MPI SETUP /////
 
 	char *mode = argv[1];
 	char *algo = argv[2];
@@ -239,7 +131,7 @@ int main(int argc, char **argv)
 				end = clock();
 				cout << "Parallel EMST Time: " << ((float)(end - start)) / CPMS << " ms\n\n";
 			}
-			tsp.dump_all_cities();
+			// tsp.dump_all_cities();
 			// tsp.fix_inversions();
 			tsp.printResults();
 		}
@@ -247,12 +139,6 @@ int main(int argc, char **argv)
 		if (mode_flag == 0 || algo_flag == 0)
 			usage();
 	}
-
-	// Finalize the MPI environment
-	MPI_Comm_free(&comm2D);
-	MPI_Comm_free(&comm1D_row);
-	MPI_Comm_free(&comm1D_col);
-	MPI_Finalize();
 
 	return 0;
 }
